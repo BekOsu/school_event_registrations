882c1d6148c895505dcbbadaf3955c3a48face04
abu <abubakersuliman@outlook.com>
2023-09-06 01:08:37 +0400

    add events app urls

--- event_registration/event_registration/urls.py
+++ event_registration/event_registration/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('events.urls')),
-                  path('', include('participants.urls')),
-                  path('', include('CRM.urls')),
-                  path("accounts/", include("django.contrib.auth.urls")),  # for users login/logout
+                  # path('', include('participants.urls')),
+                  path('', include('events.urls')),
+                  path('', RedirectView.as_view(url='events_list/')),
+                  path('accounts/', include('django.contrib.auth.urls')),
+
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
