f0f441321e9ad47e1a2f76072daa44ee7c5af0f6
abu <abubakersuliman@outlook.com>
2023-09-06 01:10:34 +0400

    add events app views

--- event_registration/events/views.py
+++ event_registration/events/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, ListView
+from django.urls import reverse_lazy
+from .forms import EventForm
+from .models import Event
 
-# Create your views here.
+
+class EventCreateView(CreateView):
+    model = Event
+    form_class = EventForm
+    template_name = 'events/event_form.html'
+    success_url = reverse_lazy('events_list')
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'events/event_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['event'] = self.object
+        return context
+
+
+class EventListView(ListView):
+    model = Event
+    template_name = 'events/event_list.html'
+    context_object_name = 'events'
+    ordering = ['-date_time']
