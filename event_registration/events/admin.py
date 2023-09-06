7ff921618c844f54579159ef21ce8cea427aaa30
abu <abubakersuliman@outlook.com>
2023-09-07 00:41:08 +0400

    register EventRegistration models

--- event_registration/events/admin.py
+++ event_registration/events/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, Participant, EventRegistration
 
 admin.site.register(Event)
+admin.site.register(Participant)
+admin.site.register(EventRegistration)
