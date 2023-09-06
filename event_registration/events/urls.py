223acda591c73689504bb2d383c3fa4a7277a23e
abu <abubakersuliman@outlook.com>
2023-09-06 21:37:00 +0400

    Refactor

--- event_registration/events/urls.py
+++ event_registration/events/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('events_list/', views.EventListView.as_view(), name='events_list'),
     path('create_event/', views.CombinedEventCreateAndCSVImportView.as_view(), name='event_create'),
-    # path('create_event/', views.EventCreateView.as_view(), name='event_create'),
     path('event/<int:pk>/', views.EventDetailView.as_view(), name='event_detail'),
 ]
