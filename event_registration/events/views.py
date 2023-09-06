3783afb1d67ae75a18b5495c65030cbf1f64c490
abu <abubakersuliman@outlook.com>
2023-09-06 16:44:13 +0400

    create CombinedEventCreateAndCSVImportView view

--- event_registration/events/views.py
+++ event_registration/events/views.py
@@ -1,18 +1,67 @@
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, FormView
 from django.urls import reverse_lazy
-from .forms import EventForm
+from .forms import EventForm, CSVUploadForm
 from .models import Event
+from django.contrib import messages
+
+from .services import parse_csv
 
 
-class EventCreateView(CreateView):
-    model = Event
+class EventFormMixin:
+    def handle_event_form(self, form):
+        form.instance.created_by = self.request.user
+        self.object = form.save()  # Explicitly set self.object after saving the form
+        messages.success(self.request, 'Event created successfully.')
+
+
+class CSVFormMixin:
+    def handle_csv_form(self, form):
+        csv_file = form.cleaned_data.get('csv_file')
+
+        imported_events = parse_csv(csv_file)
+
+        for event in imported_events:
+            event.created_by = self.request.user
+            event.save()
+
+        if imported_events:
+            self.object = imported_events[-1]  # Explicitly set self.object to the last imported event
+
+        messages.success(self.request, 'Events imported successfully.')
+
+
+class CombinedEventCreateAndCSVImportView(EventFormMixin, CSVFormMixin, CreateView, FormView):
     form_class = EventForm
-    template_name = 'events/event_form.html'
-    success_url = reverse_lazy('events_list')
+    second_form_class = CSVUploadForm
+    template_name = 'events/event_or_import.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['csv_form'] = self.second_form_class()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        if 'event_form' in request.POST:
+            self.form_class = EventForm
+            return CreateView.post(self, request, *args, **kwargs)
+        elif 'csv_form' in request.POST:
+            self.form_class = CSVUploadForm
+            return FormView.post(self, request, *args, **kwargs)
 
     def form_valid(self, form):
-        form.instance.created_by = self.request.user
-        return super().form_valid(form)
+        if isinstance(form, EventForm):
+            self.handle_event_form(form)
+            return CreateView.form_valid(self, form)
+        elif isinstance(form, CSVUploadForm):
+            self.handle_csv_form(form)
+            return FormView.form_valid(self, form)
+
+    def get_success_url(self):
+        return reverse_lazy('events_list')
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'There was an error processing the form.')
+        return super().form_invalid(form)
 
 
 class EventDetailView(DetailView):
