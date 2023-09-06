8382d04879758038075e391500bef5ca246dfa9a
abu <abubakersuliman@outlook.com>
2023-09-06 16:43:29 +0400

    config static

--- event_registration/event_registration/urls.py
+++ event_registration/event_registration/urls.py
@@ -8,8 +8,11 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('events.urls')),
                   # path('', include('participants.urls')),
-                  path('', include('events.urls')),
+                  path('', include('CRM.urls')),
                   path('', RedirectView.as_view(url='events_list/')),
                   path('accounts/', include('django.contrib.auth.urls')),
 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
