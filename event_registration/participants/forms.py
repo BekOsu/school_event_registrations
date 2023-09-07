d6b34d67735add78df53be6342a89d0a27460d1c
abu <abubakersuliman@outlook.com>
2023-09-07 10:19:37 +0400

    add placeholders

--- event_registration/participants/forms.py
+++ event_registration/participants/forms.py
@@ -6,3 +6,9 @@ class ParticipantForm(forms.ModelForm):
     class Meta:
         model = Participant
         fields = ['first_name', 'last_name', 'email', 'phone_number', 'profile_picture']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+            'phone_number': forms.TextInput(attrs={'placeholder': 'Phone Number'}),
+        }
