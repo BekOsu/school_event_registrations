b547ae4efbc4fa26fcbf9e060d742819bafe1ace
abu <abubakersuliman@outlook.com>
2023-09-07 21:13:44 +0400

    user profile page

--- event_registration/participants/urls.py
+++ event_registration/participants/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 
     path('register/<int:event_id>/', views.ParticipantCreateView.as_view(), name='participant_register'),
+    path('user_events/', views.UserEventListView.as_view(), name='user_events'),
+
 ]
--- event_registration/participants/views.py
+++ event_registration/participants/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from events.models import Participant, Event, EventRegistration
 from .forms import ParticipantForm
@@ -6,6 +7,7 @@ from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class ParticipantCreateView(LoginRequiredMixin, CreateView):
@@ -64,4 +66,31 @@ class ParticipantCreateView(LoginRequiredMixin, CreateView):
         return HttpResponseRedirect(self.get_success_url())
 
     def get_success_url(self):
-        return reverse_lazy('events_list')
+        return reverse_lazy('user_events')
+
+
+class UserEventListView(LoginRequiredMixin, ListView):
+    model = Event
+    template_name = 'participants/participant_event_list.html'
+    context_object_name = 'events'
+
+    def get_queryset(self):
+        user = self.request.user
+        try:
+            self.participant = Participant.objects.get(user=user)
+            return Event.objects.filter(participants=self.participant).order_by('date_time')
+        except ObjectDoesNotExist:
+            self.participant = None
+            return Event.objects.none()  # Returns an empty queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        try:
+            participant = Participant.objects.get(user=user)
+            context['participant'] = participant
+        except Participant.DoesNotExist:
+            context['participant'] = None
+        context['user'] = user
+        return context
+
