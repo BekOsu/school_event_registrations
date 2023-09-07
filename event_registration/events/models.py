2c2838d9815ca7e85ba359a0a310854caa479b6a
abu <abubakersuliman@outlook.com>
2023-09-07 10:29:37 +0400

    default image

--- event_registration/events/models.py
+++ event_registration/events/models.py
@@ -8,7 +8,7 @@ import datetime
 
 class Event(models.Model):
     name = models.CharField(max_length=200, unique=True)
-    image = models.ImageField(upload_to='events/', default='default_image.png')
+    image = models.ImageField(upload_to='events/', default='default_event.png')
     date_time = models.DateTimeField()
     location = models.CharField(max_length=200)
     description = models.TextField(max_length=600)
@@ -47,7 +47,7 @@ class Participant(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', default='default_user.png',  null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
