e44e82412840e3e34f1db4814ea2329c0c0277f4
abu <abubakersuliman@outlook.com>
2023-09-07 08:48:56 +0400

    participant cannot register for same event

--- event_registration/participants/views.py
+++ event_registration/participants/views.py
@@ -6,6 +6,8 @@ from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 
 
 class ParticipantCreateView(LoginRequiredMixin, CreateView):
@@ -24,21 +26,35 @@ class ParticipantCreateView(LoginRequiredMixin, CreateView):
         event_id = self.kwargs.get('event_id')
         event = get_object_or_404(Event, id=event_id)
 
+        # Check if a participant object exists for this user or create one
         participant, created = Participant.objects.get_or_create(user=self.request.user)
 
         if created:
-            messages.success(self.request, f"You have been registered for the event : {event.name}.")
+            # Populate the participant object with form data
+            participant.first_name = form.cleaned_data['first_name']
+            participant.last_name = form.cleaned_data['last_name']
+            participant.email = form.cleaned_data['email']
+            participant.phone_number = form.cleaned_data['phone_number']
+            participant.profile_picture = form.cleaned_data['profile_picture']
+            try:
+                participant.save()
+            except IntegrityError:
+                messages.error(self.request, "This email is already in use.")
+                return self.form_invalid(form)
+
+            messages.success(self.request, f"You have been registered for the event: {event.name}.")
 
         # Check if user already registered
         if EventRegistration.objects.filter(event=event, participant=participant).exists():
             messages.error(self.request, "You have already registered for this event.")
             return self.form_invalid(form)
 
+        # Check maximum participants
         if event.remaining_participants <= 0:
             messages.error(self.request, "The event has reached its maximum number of participants.")
             return self.form_invalid(form)
 
-        participant = form.save()
+        # Create the Event Registration
         EventRegistration.objects.create(event=event, participant=participant)
 
         return HttpResponseRedirect(self.get_success_url())
