e504cd116b0ef99bfcd79a7133b3ecfe88a039b2
abu <abubakersuliman@outlook.com>
2023-09-08 12:23:21 +0400

    Cache for 15 minutes

--- event_registration/events/views.py
+++ event_registration/events/views.py
@@ -84,7 +84,7 @@ class EventDetailView(DetailView):
         return context
 
 
-@method_decorator(cache_page(60 * 15), name='dispatch')  # Cache for 15 minutes
+# @method_decorator(cache_page(60 * 15), name='dispatch')  # Cache for 15 minutes
 class EventListView(ListView):
     model = Event
     template_name = 'events/event_list.html'
