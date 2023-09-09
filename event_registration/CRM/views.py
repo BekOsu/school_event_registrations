e3990fe5b7e70da66d588a9b4a0be6d4c9eda5b5
abu <abubakersuliman@outlook.com>
2023-09-09 15:22:05 +0400

    Refactor url

--- event_registration/CRM/views.py
+++ event_registration/CRM/views.py
@@ -6,7 +6,7 @@ from django.views.generic import CreateView, TemplateView
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('account_login')
     template_name = 'CRM/signup.html'
 
 
